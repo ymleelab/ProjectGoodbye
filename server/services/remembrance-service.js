f90e222b3f0ef586eb345e3948ecde56d867eb6d
dsyhwk1016 <kjsjsj1109@naver.com>
2022-07-07 02:04:53 +0900

    feature: update remembrance service - add comment func

--- server/services/remembrance-service.js
+++ server/services/remembrance-service.js
@@ -41,6 +41,52 @@ class RemembranceService {
 
         return remembrance;
     }
+
+    // 특정 추모글 조회
+    async getCommentById(remembranceId, commentId) {
+        const comment = await this.remembranceModel.findCommentById(
+            remembranceId,
+            commentId,
+        );
+        if (!comment) {
+            throw new Error(
+                '해당 추모 글은 존재하지 않습니다. 다시 확인해 주세요.',
+            );
+        }
+
+        return comment;
+    }
+
+    // 추모글 추가
+    async addComment(remembranceId, commentInfo) {
+        const comment = await this.remembranceModel.createComment(
+            remembranceId,
+            commentInfo,
+        );
+
+        return comment;
+    }
+
+    // 추모글 수정
+    async updateCommet(remembranceId, commentId, update) {
+        const updatedRemembrance = await this.remembranceModel.updateComment(
+            remembranceId,
+            commentId,
+            update,
+        );
+
+        return updatedRemembrance;
+    }
+
+    // 추모글 삭제
+    async deleteComment(remembranceId, commentId) {
+        const deletedRemembrance = await this.remembranceModel.delete(
+            remembranceId,
+            commentId,
+        );
+
+        return deletedRemembrance;
+    }
 }
 
 const remembranceService = new RemembranceService(remembranceModel);
