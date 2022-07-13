442609c998946da995654428b736ec8af1df0eae
ymleelab <ymleelab@gmail.com>
2022-07-13 23:51:11 +0900

    refactor: toolkit으로 변경

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,36 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
-import {
-	applyMiddleware,
-	legacy_createStore as createStore,
-	compose,
-} from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
 
-import reducer from '../reducers';
-import rootSaga from '../sagas';
+import rootReducer from '../reducers';
 
-const loggerMiddleware =
-	({ dispatch, getState }) =>
-	(next) =>
-	(action) => {
-		console.log(action);
-		return next(action);
-	};
+const store = () =>
+	configureStore({
+		reducer: rootReducer,
+		devTools: process.env.NODE_ENV !== 'production',
+	});
 
-const configureStore = () => {
-	const sagaMiddleware = createSagaMiddleware();
-	const middlewares = [sagaMiddleware, loggerMiddleware];
-	const enhancer =
-		process.env.NODE_ENV === 'production'
-			? compose(applyMiddleware(...middlewares))
-			: composeWithDevTools(applyMiddleware(...middlewares));
-	const store = createStore(reducer, enhancer);
-	store.sagaTask = sagaMiddleware.run(rootSaga);
-	return store;
-};
-
-const wrapper = createWrapper(configureStore, {
+const wrapper = createWrapper(store, {
 	debug: process.env.NODE_ENV === 'development',
 });
 
