7e098d754cb526c727316d0d3d5784dee2f31431
shbaek1997 <shbaek1997@gmail.com>
2022-07-13 13:24:57 +0900

    Fix: error-handler 코드 수정

--- server/src/middlewares/error-handler.ts
+++ server/src/middlewares/error-handler.ts
@@ -1,6 +1,15 @@
-function errorHandler(error, req, res, next){
+import { Request, Response, NextFunction } from 'express';
+
+function errorHandler(
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
     console.log('\x1b[33m%s\x1b[0m', error.stack);
-    res.status(400).json({result:'error', reason: error.message });
+    res.status(400).json({ result: 'error', reason: error.message });
 }
 
 export { errorHandler };
+
+
