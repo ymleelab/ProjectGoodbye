8a63aaffd8e6401b4e34f381443993b6b29df977
ymleelab <ymleelab@gmail.com>
2022-07-14 11:30:07 +0900

    feature: 로그아웃 기능 다시 추가

---
+++ frontend/src/pages/sign_out.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+
+const SignOut = () => {
+	useEffect(() => {
+		sessionStorage.removeItem('token');
+		sessionStorage.removeItem('userId');
+
+		Router.replace('/');
+	});
+};
+
+export default SignOut;
