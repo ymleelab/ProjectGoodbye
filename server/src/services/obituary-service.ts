f912e136e9f3f711c97ccd7ead3fbdfb757675bb
dsyhwk1016 <kjsjsj1109@naver.com>
2022-07-18 18:46:21 +0900

    feature: 부고 service 작성

---
+++ server/src/services/obituary-service.ts
@@ -0,0 +1,111 @@
+import bcrypt from 'bcrypt';
+import { IUpdateObituary, obituaryModel, ObituaryModel } from '../db';
+import { IFamily, IObituary } from '../db/schemas/obituary-schema';
+
+export interface ICreateObituary {
+    deceased: string;
+    dateOfBirth: string;
+    dateOfDeath: string;
+    family: Array<IFamily>;
+    funeral: string;
+    dateOfCremate: string;
+    password: string;
+}
+
+const saltOrRounds = Number(process.env.SALT_OR_ROUNDS);
+
+class ObituaryService {
+    obituaryModel: ObituaryModel;
+
+    constructor() {
+        this.obituaryModel = obituaryModel;
+    }
+
+    // 새 부고 생성
+    async addObituary(obituaryInfo: ICreateObituary): Promise<IObituary> {
+        const { dateOfBirth, dateOfDeath, password } = obituaryInfo;
+
+        const age =
+            new Date(dateOfDeath).getFullYear() -
+            new Date(dateOfBirth).getFullYear() +
+            1;
+        const hashedPassword = await bcrypt.hash(
+            password as string,
+            Number(saltOrRounds),
+        );
+
+        const newInfo = {
+            ...obituaryInfo,
+            age,
+            password: hashedPassword,
+        };
+
+        const obituary = await this.obituaryModel.create(newInfo);
+
+        return obituary;
+    }
+
+    // 특정 부고 조회
+    async getObituaryById(obituaryId: string): Promise<IObituary> {
+        const obituary = await this.obituaryModel.findById(obituaryId);
+
+        return obituary;
+    }
+
+    // 부고 수정
+    async setObituary(
+        obituaryId: string,
+        currentPassword: string,
+        update: IUpdateObituary,
+    ): Promise<IObituary> {
+        // 해당 부고의 비밀번호 조회
+        const obituary = await this.getObituaryById(obituaryId);
+        const hashedPassword = obituary.password;
+
+        const isPasswordCorrect = await bcrypt.compare(
+            currentPassword,
+            hashedPassword,
+        );
+        if (!isPasswordCorrect) {
+            throw new Error(
+                '비밀번호가 일치하지 않습니다. 다시 확인해 주세요.',
+            );
+        }
+
+        const updatedObituary = await this.obituaryModel.update(
+            obituaryId,
+            update,
+        );
+
+        return updatedObituary;
+    }
+
+    // 부고 삭제
+    async deleteObituary(
+        obituaryId: string,
+        currentPassword: string,
+    ): Promise<object> {
+        // 해당 부고의 비밀번호 조회
+        const obituary = await this.getObituaryById(obituaryId);
+        const hashedPassword = obituary.password;
+
+        const isPasswordCorrect = await bcrypt.compare(
+            currentPassword,
+            hashedPassword,
+        );
+        if (!isPasswordCorrect) {
+            throw new Error(
+                '비밀번호가 일치하지 않습니다. 다시 확인해 주세요.',
+            );
+        }
+
+        const deletedObituary = await this.obituaryModel.delete(obituaryId);
+        const status = deletedObituary ? 'success' : 'fail';
+
+        return { result: status };
+    }
+}
+
+const obituaryService = new ObituaryService();
+
+export { obituaryService };
