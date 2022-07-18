57d4fc15a7e7c5d4543d3c96480feb638ac44936
ymleelab <ymleelab@gmail.com>
2022-07-19 00:05:32 +0900

    design: 버튼 hover시 배경색 변경

--- frontend/src/util/common_styles.js
+++ frontend/src/util/common_styles.js
@@ -2,9 +2,13 @@ import styled from '@emotion/styled';
 
 export const Button = styled.button`
 	color: #3e606f;
-	background-color: #d1dbbd;
-	border: none;
+	background-color: #fff;
+	border: 2px solid #d1dbbd;
 	cursor: pointer;
 	border-radius: 0.2rem;
 	padding: 0.3rem 0.5rem;
+
+	&:hover {
+		background-color: #d1dbbd;
+	}
 `;
