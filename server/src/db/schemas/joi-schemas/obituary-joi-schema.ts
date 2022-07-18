f63fa5b89ff1af6eabf09945853eb95c9f13dcb5
dsyhwk1016 <kjsjsj1109@naver.com>
2022-07-18 18:45:44 +0900

    feature: 날짜 형식 검사 강화

--- server/src/db/schemas/joi-schemas/obituary-joi-schema.ts
+++ server/src/db/schemas/joi-schemas/obituary-joi-schema.ts
@@ -2,22 +2,27 @@ import joi from 'joi';
 
 const createObituaryJoiSchema = joi.object({
     deceased: joi.string().required(),
-    dateOfBirth: joi.date().required(),
-    dateOfDeath: joi.date().required(),
+    dateOfBirth: joi.date().max(joi.ref('dateOfDeath')).iso().required(),
+    dateOfDeath: joi
+        .date()
+        .min(joi.ref('dateOfBirth'))
+        .max('now')
+        .iso()
+        .required(),
     age: joi.number().required(),
     family: joi.array().required(),
     funeral: joi.string().required(),
-    dateOfCremate: joi.date().required(),
+    dateOfCremate: joi.date().min(joi.ref('dateOfDeath')).iso().required(),
 });
 
 const updateObituaryJoiSchema = joi.object({
     deceased: joi.string(),
-    dateOfBirth: joi.date(),
-    dateOfDeath: joi.date(),
+    dateOfBirth: joi.date().max(joi.ref('dateOfDeath')).iso(),
+    dateOfDeath: joi.date().min(joi.ref('dateOfBirth')).max('now').iso(),
     age: joi.number(),
     family: joi.array(),
     funeral: joi.string(),
-    dateOfCremate: joi.date(),
+    dateOfCremate: joi.date().min(joi.ref('dateOfDeath')).iso(),
 });
 
 export { createObituaryJoiSchema, updateObituaryJoiSchema };
