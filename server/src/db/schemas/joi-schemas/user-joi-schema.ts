3cb8463a700099ed8211f3603143b8ee096be727
shbaek1997 <shbaek1997@gmail.com>
2022-07-21 11:20:59 +0900

    Feat:joi custom error message 추가

--- server/src/db/schemas/joi-schemas/user-joi-schema.ts
+++ server/src/db/schemas/joi-schemas/user-joi-schema.ts
@@ -1,19 +1,48 @@
 import joi from 'joi';
 
 const registerJoiSchema = joi.object({
-    email: joi.string().email().lowercase().required(),
-    fullName: joi.string().required(),
-    password:joi.string().required().min(8),
-    repeatPassword:joi.string().required().min(8),
-    dateOfBirth: joi.date().required(),
+    email: joi.string().email().lowercase().required().messages({
+        'string.empty': '이메일은 비어있을 수 없습니다.',
+        'any.required': '이메일은 반드시 입력되어야 합니다.',
+        'string.email': '올바르지 않은 이메일 형식입니다.',
+    }),
+    fullName: joi.string().required().messages({
+        'string.empty': '이름은 비어있을 수 없습니다.',
+        'any.required': '이름은 반드시 입력되어야 합니다.',
+    }),
+    password:joi.string().required().min(8).messages({
+        'string.empty': '비밀번호는 비어있을 수 없습니다.',
+        'any.required': '비밀번호는 반드시 입력되어야 합니다.',
+        'string.min': '비밀번호는 최소 8글자 이상이어야 합니다.',
+    }),
+    repeatPassword:joi.string().required().min(8).messages({
+        'string.empty': '비밀번호는 비어있을 수 없습니다.',
+        'any.required': '비밀번호는 반드시 입력되어야 합니다.',
+        'string.min': '비밀번호는 최소 8글자 이상이어야 합니다.',
+    }),
+    dateOfBirth: joi.date().required().iso().min('1900-01-01').messages({
+        'any.required': '생년월일은 반드시 입력되어야 합니다.',
+        'date.base': '올바른 날짜 형식이 아닙니다.',
+    }),
 });
 
 const userUpdateJoiSchema = joi.object({
-    fullName:joi.string(),
+    fullName:joi.string().messages({
+        'string.empty': '이름은 비어있을 수 없습니다.',
+    }),
     password: joi.string().min(8),
-    currentPassword: joi.string().min(8),
-    dateOfBirth: joi.date(),
-    photo: joi.string().uri(),
+    currentPassword: joi.string().min(8).required().messages({
+        'string.empty': '현재 비밀번호는 비어있을 수 없습니다.',
+        'any.required': '현재 비밀번호는 반드시 입력되어야 합니다.',
+        'string.min': '비밀번호는 최소 8글자 이상이어야 합니다.',
+    }),
+    dateOfBirth: joi.date().iso().min('1900-01-01').messages({
+        'date.base': '올바른 날짜 형식이 아닙니다.',
+    }),
+    photo: joi.string().uri().messages({
+        'string.empty': '사진은 비어있을 수 없습니다.',
+        'string.uri': '올바르지 않은 url 형식입니다.',
+    }),
 
 })
 
--- server/src/db/schemas/joi-schemas/will-joi-schema.ts
+++ server/src/db/schemas/joi-schemas/will-joi-schema.ts
@@ -1,8 +1,16 @@
 import joi from 'joi';
 
 const createWillJoiSchema = joi.object({
-    title: joi.string().required().min(10), // 10자 이상 ?
-    content: joi.string().required().min(10), // 10자 이상
+    title: joi.string().required().min(10).messages({
+        'string.empty': '유언장 제목은 비어있을 수 없습니다.',
+        'any.required': '유언장 제목은 반드시 입력되어야 합니다.',
+        'string.min': '유언장 제목은 최소 10글자 이상이어야 합니다.',
+    }), // 10자 이상 ?
+    content: joi.string().required().min(10).messages({
+        'string.empty': '유언장 내용은 비어있을 수 없습니다.',
+        'any.required': '유언장 내용은 반드시 입력되어야 합니다.',
+        'string.min': '유언장 내용은 최소 10글자 이상이어야 합니다.',
+    }), // 10자 이상
     receivers: joi.array().items(
         joi.object({
             receiverId: joi.string(),
@@ -12,12 +20,23 @@ const createWillJoiSchema = joi.object({
 });
 
 const updateWillJoiSchema = joi.object({
-    title: joi.string().min(10), // 10자 이상 ?
-    content: joi.string().min(10), // 10자 이상
+    title: joi.string().min(10).messages({
+        'string.empty': '유언장 제목은 비어있을 수 없습니다.',
+        'string.min': '유언장 제목은 최소 10글자 이상이어야 합니다.',
+    }), // 10자 이상 ?
+    content: joi.string().min(10).messages({
+        'string.empty': '유언장 내용은 비어있을 수 없습니다.',
+        'string.min': '유언장 내용은 최소 10글자 이상이어야 합니다.',
+    }), // 10자 이상
     receivers: joi.array().items(
         joi.object({
-            receiverId: joi.string(),
-            email: joi.string().email(),
+            receiverId: joi.string().messages({
+                'string.empty': '수신자 아이디는 비어있을 수 없습니다.',
+            }),
+            email: joi.string().email().messages({
+                'string.empty': '수신자 이메일은 비어있을 수 없습니다.',
+                'string.email': '올바르지 않은 이메일 형식입니다.',
+            }),
         }),
     ),
 });
