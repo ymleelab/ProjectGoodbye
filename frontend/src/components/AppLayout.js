98414906dbf8c79b0a5b2f2f29cf6fc79f734ede
ymleelab <ymleelab@gmail.com>
2022-07-23 01:47:48 +0900

    design: 배경색 버튼 어두운색과 동일하게 변경

--- frontend/src/components/AppLayout.js
+++ frontend/src/components/AppLayout.js
@@ -30,7 +30,7 @@ const Wrapper = styled.div`
 	position: relative;
 	padding-bottom: 200px;
 	font-family: 'Noto Sans KR', sans-serif;
-	background-image: radial-gradient(circle at 50% 50%, #16461b, #fff);
+	background-image: radial-gradient(circle at 50% 50%, #6e8e7d, #fff);
 `;
 
 const Cursor = styled.div`
